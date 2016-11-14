c63fdaa8639a729585a87fb32846b11ec328581b
emanseid <emanseid@gmail.com>
2016-11-14 15:46:58 -0500

    work

---
+++ Chapter3/src/edu/gsu/cis/ch3/Question19.java
@@ -0,0 +1,26 @@
+package edu.gsu.cis.ch3;
+import java.util.Scanner;
+
+public class Question19 {
+	public static void main(String[]args){
+		
+		Scanner input= new Scanner(System.in);
+		int p;
+		System.out.println("Enter three edges of a triangle");
+		
+		int a = input.nextInt();
+		int b = input.nextInt();
+		int c = input.nextInt();
+		
+		if(a+b>c&&b+c>a&&a+c>b){
+			 p = a+b+c;
+		System.out.println("The perimeter of the triangle is  "+p);}
+		
+		else
+			System.out.println("invalid input");
+		
+		
+		
+	}
+
+}
