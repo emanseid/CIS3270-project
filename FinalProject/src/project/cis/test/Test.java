1971725349df1cfd4ddcecd17c91bfc072eea4cc
Jack Bae <jbae6@student.gsu.edu>
2016-12-04 16:29:07 -0500

    Splash Screen. Need to change image and image path for use.

--- FinalProject/src/project/cis/test/Test.java
+++ FinalProject/src/project/cis/test/Test.java
@@ -1,13 +1,24 @@
 package project.cis.test;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JWindow;
+import javax.swing.SwingConstants;
+
 public class Test {
-	public static void main(String [] args){
-		//testing pull/push - Brooke
-		//Eman- want to try editing this?
-		//Added by Jack2
-		//Hi - new push
-		//Let's try one more
-		//yes
-		//yes2
+	public static void main(String[] args){
+		
+		JWindow window = new JWindow();
+		//Creating and importing the splash screen
+		window.getContentPane().add(new JLabel("",new ImageIcon("/Users/jackj/Desktop/plane.jpg"),SwingConstants.CENTER));
+		// Location of Splash Screen
+		window.setBounds(450,300,320,240);
+		window.setVisible(true);
+		// Setting how long splash screen will display
+		try{
+			Thread.sleep(5000);
+		}catch(InterruptedException e){}
+		// Closing the splash screen
+		window.dispose();
 	}
 }
