4f29421fcec601a1ff0c376c25e16615f2be1cdb
emanseid <eseid1@student.gsu.edu>
2016-12-13 17:04:35 -0500

    Final Menu

--- FinalProject/src/project/cis/test/Menu.java
+++ FinalProject/src/project/cis/test/Menu.java
@@ -1,52 +1,63 @@
 package project.cis3270.searchflight;
 import javax.swing.*;
+
+import project.cis3270.searchflight.NewUserRegistration.NewUserListener;
+
 import java.awt.event.*;
 
-public class Menu implements ActionListener{
+
+public class Menu extends NewUserRegistration implements ActionListener{
 	 static JFrame fr;
 	 JPanel panel;
 	 JMenuBar mb;
 	 JMenu m;
-	 JMenuItem search, booking, login,MyAccount,register;
+	 JMenuItem search, booking;
 	 
 	 public Menu() {
+		 //creating the frame
 		 fr = new JFrame();
+		 // creating the menu bar
 		 mb = new JMenuBar();
-		 m = new JMenu("Project");
+		 //naming and creating the menu tab
+		 m = new JMenu("Book Flight");
+		 //creating the list to be included
 		 search = new JMenuItem("Search");
 		 booking = new JMenuItem("Booking");
-		 login = new JMenuItem("Login");
-		 register=new JMenuItem("register");
-		 MyAccount= new JMenuItem("MyAccount");
 		 
+		
+		 
+		 //creating a panel
 		 panel = new JPanel();
+		 //adding the panel to the frame
 		 fr.getContentPane().add(panel);
-		 
+		 //adding all the items to the menu
 		 panel.add(mb);
 		 mb.add(m);
 		 m.add(search);
 		 m.add(booking);
-		 m.add(login);
-		 m.add(MyAccount);
+		
+		
+		 
 		 search.addActionListener(this);
 		 booking.addActionListener(this);
-		 MyAccount.addActionListener(this);
+		 
 	 }
-	 
+	 //event handling for Search flight
 	 public void actionPerformed(ActionEvent e1) {
 		 if(e1.getSource() == search) {
 			SearchFlight obj = new SearchFlight(); 
 			panel.add(obj);
 		 }
+		 //event handling for booking
 		 else if(e1.getSource() == booking) {
 				Booking obj = new Booking(); 
 				panel.add(obj);
 			 }
-		 else if(e1.getSource()== login){
+		
 			 
 		 }
-	 }
 	 
+	 //display
 	 public static void main(String[] args) {
 		 Menu obj = new Menu();
 		fr.setVisible(true);
