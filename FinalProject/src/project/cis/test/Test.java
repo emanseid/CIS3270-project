6525a5da2436fd781a65271bf0cc8148e2e193fd
jbae6 <jbae6@student.gsu.edu>
2016-12-13 20:38:22 -0500

    Test update

--- FinalProject/src/project/cis/test/Test.java
+++ FinalProject/src/project/cis/test/Test.java
@@ -5,20 +5,26 @@ import javax.swing.JLabel;
 import javax.swing.JWindow;
 import javax.swing.SwingConstants;
 
-public class Test {
+public class Test extends MainMenu {
 	public static void main(String[] args){
 		
 		JWindow window = new JWindow();
 		//Creating and importing the splash screen
-		window.getContentPane().add(new JLabel("",new ImageIcon("/Users/jackj/Desktop/plane.jpg"),SwingConstants.CENTER));
+		window.getContentPane().add(new JLabel("",new ImageIcon("/Users/Brooke/Desktop/javaAir.jpg"),SwingConstants.CENTER));
 		// Location of Splash Screen
-		window.setBounds(450,300,320,240);
+		window.setBounds(100,500,1000,625);
 		window.setVisible(true);
+		window.setLocationRelativeTo(null);
 		// Setting how long splash screen will display
 		try{
 			Thread.sleep(5000);
 		}catch(InterruptedException e){}
 		// Closing the splash screen
 		window.dispose();
+		
+		MainMenu obj = new MainMenu();
+		fr.setVisible(true);
+		fr.pack();
+		fr.setLocationRelativeTo(null);
 	}
 }
